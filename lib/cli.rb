bf7d69047e40856f6b443bdfd251d29ca9d3c625
Maxine <maxinemeurer@yahoo.com>
2021-02-25 14:35:31 -0500

    added in instance methods in apiand called them in the cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,4 @@
 # gets inputs and will show strings/ display
-require_relative '../lib/api.rb'
 
 class PlantFinder::CLI
     def run
@@ -10,14 +9,16 @@ class PlantFinder::CLI
 
     def greeting
         puts "\n Welcome to Plant Base!"
-        puts "\n ---___---Loading Data---___---"
+        puts "\n ---___--- * Loading Data * ---___---"
     end
 
     def input
-        puts " \nPlease enter the common name of a plant species."
+        puts " \nPlease enter the common name of a plant species:"
         name = gets.chomp
-        puts PlantFinder::Plant.find_by_common_name(name)
-
+        choosen_plant = PlantFinder::Plant.find_by_common_name(name)
+        puts "\n-Scientific name: #{choosen_plant.scientific_name} \n
+-Family common name: #{choosen_plant.family_common_name}\n
+-Genus: #{choosen_plant.genus} \n"
     end
 
 end
--- lib/plant.rb
+++ lib/plant.rb
@@ -9,9 +9,24 @@ class PlantFinder::Plant
         @genus = plant_hash["genus"]
         @@plants << self
     end
-
+    # will return the first plant object / class method  down below
     def self.find_by_common_name(name) 
         @@plants.find {|plant| plant.common_name.downcase == name.downcase}
     end
+    #instance method 
+    def common_name
+        @common_name
+    end
 
+    def scientific_name
+        @scientific_name
+    end
+
+    def family_common_name
+        @family_common_name
+    end
+
+    def genus
+        @genus
+    end
 end
