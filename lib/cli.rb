67d73f90a96eb59b48a7f616701ca3ebe158ed25
Maxine <maxinemeurer@yahoo.com>
2021-02-25 12:57:01 -0500

    redefined find_by_common_name method in plant.rb

--- lib/cli.rb
+++ lib/cli.rb
@@ -9,14 +9,11 @@ class PlantFinder::CLI
     end
 
     def greeting
-        puts "\n Welcome to Plant Finder!"
+        puts "\n Welcome to Plant Base!"
         puts "\n ---___---Loading Data---___---"
     end
 
     def menu
-        puts " \nPlease choose between the common name and keyword search. 
-If you are unsure of the plant's exact common name, refer to the keyword search. \n "
-#For example, if you wanted to know more about a specific palm tree, 
-#but you're not sure of the common name, simply enter 'palm', as you would 'pine' for pine trees.\n "
+        puts " \nPlease enter the common name of a plant species."
     end
 end
--- lib/plant.rb
+++ lib/plant.rb
@@ -1,15 +1,17 @@
 # This is where the data will be stored
 class PlantFinder::Plant
         @@plants = []
-    def initialize(plant_hash=nil)
+        attr_reader :common_name, :scientific_name, :family_common_name, :genus
+    def initialize(plant_hash)
         @common_name = plant_hash["common_name"]
         @scientific_name = plant_hash["scientific_name"]
         @family_common_name = plant_hash["family_common_name"]
         @genus = plant_hash["genus"]
+        @@plants << self
     end
 
-    def find_by_common_name(name) 
-        @common_name.find(name)
+    def self.find_by_common_name(name) 
+        @@plants.find {|plant| plant.common_name.downcase == name.downcase}
         binding.pry
     end
 
