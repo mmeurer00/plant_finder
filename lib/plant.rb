b60124d90410c0dd67ecf11492d01a13e95994df
Maxine <maxinemeurer@yahoo.com>
2021-02-24 23:27:38 -0500

    adding a method that organizes the specific keys I want to display from the hash

--- lib/plant.rb
+++ lib/plant.rb
@@ -1,17 +1,23 @@
 # This is where the data will be stored
 class PlantFinder::Plant
         @@plants = []
-    def initialize(plant_hash=nil)
+    def initialize(plant_hash = nil)
         @@plants << plant_hash
         #binding.pry
     end
 
+    def organized_plant_hash
+        keys = ["common_name", "scientific_name"]
+        @@plants.map do |hash|
+            hash.select { |k, _| keys.include? k }
+        end
+    end
 
     def find_by_common_name(name)
         @@plants.each do |hash| 
             if hash["common_name"] == name
                 hash.each do |key, value|
-                    puts "#{key} = #{value}"
+                    puts "#{key}= #{value}"
                 end
             end
         end
